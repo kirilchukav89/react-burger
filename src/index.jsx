dc9001d0ba820b872df5b5bd7c5bf61bb5b713d0
Aleksey Kirilchuk <a.v.kirilchuk@yandex.ru>
2024-06-24 15:54:30 +0300

    Create store

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,34 @@
 // import React from 'react';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const preloadedState = {
+  allIngredients: [],
+  currentIngredients: [],
+  viewIngredient: {},
+  order: {}
+};
+
+const reducer = (state, action) => {
+
+}
+
+const store = configureStore({
+  reducer,
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState
+});
+
 root.render(
   // <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   // </React.StrictMode>
 );
